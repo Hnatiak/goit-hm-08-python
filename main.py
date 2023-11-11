60131ec14146b7d9b0d6de1be6dfe8c99e0f902e
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-11-11 14:19:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,52 +64,52 @@ if __name__ == "__main__":
 
 
 
-from datetime import date, timedelta
+# from datetime import date, timedelta
 
-def get_birthdays_per_week(users):
-    # Сьогоднішня дата
-    today = date.today()
+# def get_birthdays_per_week(users):
+#     # Сьогоднішня дата
+#     today = date.today()
 
-    # Список днів тижня в порядку
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+#     # Список днів тижня в порядку
+#     days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-    # Створюємо словник для збереження днів народження
-    birthdays_per_week = {day: [] for day in days_of_week}
+#     # Створюємо словник для збереження днів народження
+#     birthdays_per_week = {day: [] for day in days_of_week}
 
-    # Проходимося по користувачам і додаємо їх до словника, якщо їх день народження в цьому тижні
-    for user in users:
-        user_name = user['name']
-        user_birthday = user['birthday']
+#     # Проходимося по користувачам і додаємо їх до словника, якщо їх день народження в цьому тижні
+#     for user in users:
+#         user_name = user['name']
+#         user_birthday = user['birthday']
 
-        # Обчислюємо день тижня для дня народження користувача
-        birthday_weekday = user_birthday.weekday()
+#         # Обчислюємо день тижня для дня народження користувача
+#         birthday_weekday = user_birthday.weekday()
 
-        # Визначаємо різницю в днях між сьогоднішньою датою і днем народження
-        days_until_birthday = (user_birthday - today).days
+#         # Визначаємо різницю в днях між сьогоднішньою датою і днем народження
+#         days_until_birthday = (user_birthday - today).days
 
-        # Перевіряємо, чи день народження в цьому тижні і чи не минув вже цього року
-        if 0 <= birthday_weekday < 5 and days_until_birthday >= 0:
-            # Додаємо користувача до відповідного дня тижня
-            birthdays_per_week[days_of_week[birthday_weekday]].append(user_name)
+#         # Перевіряємо, чи день народження в цьому тижні і чи не минув вже цього року
+#         if 0 <= birthday_weekday < 5 and days_until_birthday >= 0:
+#             # Додаємо користувача до відповідного дня тижня
+#             birthdays_per_week[days_of_week[birthday_weekday]].append(user_name)
 
-    # Переносимо дні народження, якщо вони випадають на вихідні (Saturday, Sunday)
-    for day in ["Saturday", "Sunday"]:
-        birthdays_per_week["Monday"].extend(birthdays_per_week[day])
-        birthdays_per_week[day] = []
+#     # Переносимо дні народження, якщо вони випадають на вихідні (Saturday, Sunday)
+#     for day in ["Saturday", "Sunday"]:
+#         birthdays_per_week["Monday"].extend(birthdays_per_week[day])
+#         birthdays_per_week[day] = []
 
-    return birthdays_per_week
+#     return birthdays_per_week
 
-# Приклад використання:
-if __name__ == "__main__":
-    users = [
-        {"name": "Bill", "birthday": date(2023, 11, 14)},
-        {"name": "Jan", "birthday": date(2023, 11, 16)},
-        {"name": "Kim", "birthday": date(2023, 11, 15)},
-        {"name": "DIED", "birthday": date(1980, 11, 15)},
-        {"name": "ROMAN", "birthday": date(2024, 11, 15)},
-        {"name": "OLEG", "birthday": date(2026, 9, 15)},
-    ]
+# # Приклад використання:
+# if __name__ == "__main__":
+#     users = [
+#         {"name": "Bill", "birthday": date(2023, 11, 14)},
+#         {"name": "Jan", "birthday": date(2023, 11, 16)},
+#         {"name": "Kim", "birthday": date(2023, 11, 15)},
+#         {"name": "DIED", "birthday": date(1980, 11, 15)},
+#         {"name": "ROMAN", "birthday": date(2024, 11, 15)},
+#         {"name": "OLEG", "birthday": date(2026, 9, 15)},
+#     ]
 
-    birthdays = get_birthdays_per_week(users)
-    print(birthdays)
+#     birthdays = get_birthdays_per_week(users)
+#     print(birthdays)
 
