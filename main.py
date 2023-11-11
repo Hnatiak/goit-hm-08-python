b83299aa2d8edba0bc11dea5c22d566d8a0cad05
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-11-11 14:21:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,93 @@
 from datetime import date, datetime
 
 def get_birthdays_per_week(users):
-    
     current_date = date.today()
     current_weekday = current_date.weekday()
-    
+
     if all(user["birthday"].year > current_date.year for user in users):
         return {}
-    
+
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    birthdays_per_week = {day: [] for day in weekdays}
+
     for user in users:
         birthday = user["birthday"]
-        diff = (birthday - current_date).days
+        diff = (birthday.replace(year=current_date.year) - current_date).days
         next_birthday_weekday = (current_weekday + diff) % 7
-        day_name = list(users.keys())[next_birthday_weekday]
-        users[day_name].append(user["name"])
-    
-    return users
-    
-    # birthdays_per_week = {
-    #     'Monday': ['Bill', 'Jan'],
-    #     'Tuesday': ['Kim'],
-    #     'Wednesday': [],
-    #     'Thursday': [],
-    #     'Friday': [],
-    # }
-    
-    # print(users)
-    
-    # for day, names in users.items():
-    #     birthdays_per_week[day] = [name for name in names if name in users[day]]
-    
-    # return birthdays_per_week
+        day_name = weekdays[next_birthday_weekday]
+        birthdays_per_week[day_name].append(user["name"])
 
+    return birthdays_per_week
 
-if __name__ == "__main__":
+if __name__ == "__main":
     users = [
         {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
     ]
 
     result = get_birthdays_per_week(users)
-    print(result)
-    # Виводимо результат
+
+    # Print the result
     for day_name, names in result.items():
         print(f"{day_name}: {', '.join(names)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from datetime import date, datetime
+
+# def get_birthdays_per_week(users):
+    
+#     current_date = date.today()
+#     current_weekday = current_date.weekday()
+    
+#     if all(user["birthday"].year > current_date.year for user in users):
+#         return {}
+    
+#     for user in users:
+#         birthday = user["birthday"]
+#         diff = (birthday - current_date).days
+#         next_birthday_weekday = (current_weekday + diff) % 7
+#         day_name = list(users.keys())[next_birthday_weekday]
+#         users[day_name].append(user["name"])
+    
+#     return users
+    
+#     # birthdays_per_week = {
+#     #     'Monday': ['Bill', 'Jan'],
+#     #     'Tuesday': ['Kim'],
+#     #     'Wednesday': [],
+#     #     'Thursday': [],
+#     #     'Friday': [],
+#     # }
+    
+#     # print(users)
+    
+#     # for day, names in users.items():
+#     #     birthdays_per_week[day] = [name for name in names if name in users[day]]
+    
+#     # return birthdays_per_week
+
+
+# if __name__ == "__main__":
+#     users = [
+#         {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+#     ]
+
+#     result = get_birthdays_per_week(users)
+#     print(result)
+#     # Виводимо результат
+#     for day_name, names in result.items():
+#         print(f"{day_name}: {', '.join(names)}")
 
 
 
